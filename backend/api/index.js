9579dfe4d5ddd93a65c602bd8fc9669829db2f98
WaheedDahri <dahrivicky@gmail.com>
2026-01-18 16:27:03 +0500

    Vercel Deploying Changes

---
+++ backend/api/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import connectDB from "../config/mongodb.js";
+import connectCloudinary from "../config/cloudinary.js";
+
+import userRouter from "../routes/userRoute.js";
+import productRouter from "../routes/productRoute.js";
+import cartRouter from "../routes/cartRoute.js";
+import orderRouter from "../routes/orderRoute.js";
+
+// Initialize app
+const app = express();
+
+// Connect DB & Cloudinary
+connectDB();
+connectCloudinary();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is working 🚀");
+});
+
+export default app;
